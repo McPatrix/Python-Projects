668a9681f64a8a64c37ab6e0de882d2906409579
pcall <pcallaway0603@gmail.com>
2024-01-12 22:03:59 -0500

    Updated web scrapper w/ comments

--- TestScript.py
+++ TestScript.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import requests
 
 # Open file
 with open("index.html", "r") as f:
@@ -14,3 +15,32 @@ print(tag)
 # Print string in tag
 
 print(tag.string)
+
+# This will find the only the first occurance 
+tag2 = doc.find("a")
+print(tag2)
+
+# This will find ALL occurances
+# Adding [] will find nested items
+
+tag3 = doc.find_all("p")[0]
+print(tag3)
+
+# Using requests module
+
+url = "https://www.newegg.com/msi-geforce-rtx-3080-rtx3080-suprim-x-10g/p/N82E16814137609?Item=9SIAPMXK7E6201"
+#  Sends GET request to url variable 
+result = requests.get(url)
+
+# Prints text
+#print(result.text)
+
+# Parsing website HTML
+
+doc2 = BeautifulSoup(result.text, "html.parser")
+print(doc2.prettify())
+
+# locate Text
+
+prices = doc.find_all(str = "$")
+print(prices)
