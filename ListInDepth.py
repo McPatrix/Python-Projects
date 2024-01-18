205c47cdbe7cb1ebeb14c8c718790d3a6e53b3d0
Pat Callaway <callawayp0705@gmail.com>
2024-01-17 20:47:19 -0500

    Super rough draft of an idea WIP

--- ListInDepth.py
+++ ListInDepth.py
@@ -1,8 +1,14 @@
-Inventory = ["Sword", "Sheild", "Health Potion", "Leather Armor", "Leather Gloves", "Iron Boots",
-          "Crossbow"]
+Inventory = ["Sword", "Sheild", "Health Potion", "Leather Armor", "Leather Gloves", "Iron Boots"]
 
 print(Inventory)
-
+# Empty chest to transfer items to.
 storageChest = []
+
+for i in Inventory:
+    if Inventory[0] == "Sword":
+        Inventory.pop
+        storageChest.extend(Inventory)
+    else:
+        print("Error")
 
 print(storageChest)
